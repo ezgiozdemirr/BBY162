7479e52721d270be2b44bc24c0e26bfc00ac6923
ezgiozdemirr <36951557+ezgiozdemirr@users.noreply.github.com>
2018-03-13 23:11:26 +0300

    Add files via upload

---
+++ uygulama04.py
@@ -0,0 +1,12 @@
+erkekadı = input("Bir erkek ismi giriniz: ")
+kadınadı = input("Bir kadın ismi giriniz: ")
+dize= int(input("Dize sayısı giriniz: "))
+
+
+
+sarkı = [erkekadı + " " "olmak da ayıp değil" " " + kadınadı + " " "olmak da", "Hatta sevda yüzünden ölmek de ayıp değil" , "Bütün iş" " " + erkekadı + " " "ile" " " + kadınadı + " " "olabilmekte yani yürekte", "Mesela bir barikatta dövüşerek" " " + erkekadı , "Mesela Kuzey Kutbunu keşfe giderken" " " + kadınadı , "Mesela denerken damarlarında bir serumu ölmek ayıp olur mu?" , erkekadı + " " "olmak da ayıp değil" " " + kadınadı + " " "olmak da","Hatta sevda yüzünden ölmek de ayıp değil!"]
+
+for olusturulacak_sarkı in sarkı[:dize]:
+    print(olusturulacak_sarkı)
+
+
